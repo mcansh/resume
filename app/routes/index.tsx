8a3617d04aaf349b25bc03ca2471bf89ba76f7c7
Logan McAnsh <logan@mcan.sh>
2021-01-22 17:37:24 -0500

    chore: general style updates
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,6 @@
-import { MetaFunction } from "@remix-run/core";
+import { HeadersFunction, MetaFunction } from "@remix-run/core";
+import { Loader } from "@remix-run/data";
+import { useRouteData } from "@remix-run/react";
 
 const meta: MetaFunction = () => {
   return {
@@ -7,91 +9,98 @@ const meta: MetaFunction = () => {
   };
 };
 
-const IndexPage: React.VFC = () => {
-  return (
-    <div className="py-4 mx-auto max-w-prose">
-      <header className="flex items-center px-4 pb-2 mb-2 space-x-4 space-y-1 border-b-2 border-yellow-500 border-solid">
-        <img
-          src="https://avatars.githubusercontent.com/u/11698668?s=320&u=14a201dea37b6bdabaccd76ff4e2664b8e337052&v=4"
-          className="w-40 h-40 rounded-full"
-          alt="Logan McAnsh"
-        />
-        <div>
-          <h1 className="text-4xl font-semibold">Logan McAnsh</h1>
-          <a href="mailto:logan+resume@mcan.sh">logan+resume@mcan.sh</a>
-        </div>
-      </header>
+const loader: Loader = () => {
+  return { date: new Date() };
+};
 
-      <div className="px-4 space-y-8">
-        <div>
-          <h2 className="text-2xl font-semibold">Skills</h2>
-          <ul className="flex flex-wrap gap-2">
-            <li className="px-2 py-1 text-white bg-indigo-600 rounded">
-              Node.js
-            </li>
-            <li className="px-2 py-1 text-white bg-indigo-600 rounded">
-              Rest APIs
-            </li>
-            <li className="px-2 py-1 text-white bg-indigo-600 rounded">
-              GraphQL
-            </li>
-            <li className="px-2 py-1 text-white bg-indigo-600 rounded">
-              React
-            </li>
-            <li className="px-2 py-1 text-white bg-indigo-600 rounded">
-              Next.js
-            </li>
-            <li className="px-2 py-1 text-white bg-indigo-600 rounded">
-              Remix Run
-            </li>
-            <li className="px-2 py-1 text-white bg-indigo-600 rounded">
-              TypeScript
-            </li>
-            <li className="px-2 py-1 text-white bg-indigo-600 rounded">
-              TailwindCSS
-            </li>
-            <li className="px-2 py-1 text-white bg-indigo-600 rounded">
-              Accessibility
-            </li>
-            <li className="px-2 py-1 text-white bg-indigo-600 rounded">
-              Performance
-            </li>
-          </ul>
-        </div>
-        <div>
-          <h2 className="text-2xl font-semibold">Experience</h2>
-          <ul className="pl-6">
-            <li>
-              <div>
-                <h3 className="text-lg font-medium">Powerley</h3>
-                <span className="block">Web Developer</span>
-                <time dateTime={new Date(2016, 4, 4).toISOString()}>
-                  May 2016
-                </time>{" "}
-                - <time dateTime={new Date().toISOString()}>Present</time>
-                <ul className="pl-6 list-disc">
-                  <li>First member of the web team</li>
-                  <li>
-                    Created and maintained a suite of web experiences included
-                    in our mobile apps, including{" "}
-                    <a
-                      href="https://powerley.com/the-voice-of-the-home-infographic-the-powerley-advisor/"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Advisor
-                    </a>
-                    , a home profile tool to better let us know about your home,
-                    and a help center.
-                  </li>
-                  <li>
-                    Created a suite of utility functions used across our web
-                    experiences
-                  </li>
-                </ul>
-              </div>
-            </li>
-          </ul>
+const headers: HeadersFunction = () => {
+  return {
+    "cache-control": "max-age=600, s-maxage=600",
+    "x-hello-recruiters": "👋",
+  };
+};
+
+const skills: Array<string> = [
+  "Node.js",
+  "Rest APIs",
+  "GraphQL",
+  "React",
+  "Next.js",
+  "Remix Run",
+  "TypeScript",
+  "TailwindCSS",
+  "Accessibility",
+  "Performance",
+];
+
+interface LoaderData {
+  date: string;
+}
+
+const IndexPage: React.VFC = () => {
+  const data = useRouteData<LoaderData>();
+
+  return (
+    <div className="h-full border-t-8 border-indigo-600 border-solid">
+      <div className="py-4 mx-auto max-w-prose">
+        <header className="flex flex-col items-center px-4 pb-2 mb-2 space-x-4 space-y-1 text-center sm:flex-row sm:text-left">
+          <img
+            src="https://avatars.githubusercontent.com/u/11698668?s=320&u=14a201dea37b6bdabaccd76ff4e2664b8e337052&v=4"
+            className="w-32 h-32 rounded-full sm:w-40 sm:h-40"
+            alt="Logan McAnsh"
+          />
+          <div>
+            <h1 className="text-4xl font-semibold">Logan McAnsh</h1>
+            <a
+              className="transition-colors duration-75 ease-in-out text-blueGray-600 hover:text-blueGray-300"
+              href="mailto:logan+resume@mcan.sh"
+            >
+              logan+resume@mcan.sh
+            </a>
+          </div>
+        </header>
+
+        <div className="px-4 space-y-8">
+          <div>
+            <h2 className="text-2xl font-semibold">Skills</h2>
+            <ul className="flex flex-wrap gap-2">
+              {skills.map((skill) => (
+                <li
+                  key={skill}
+                  className="px-2 py-1 tracking-wide text-white bg-indigo-600 rounded"
+                >
+                  {skill}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <h2 className="text-2xl font-semibold">Experience</h2>
+            <ul className="pl-6">
+              <li>
+                <div>
+                  <h3 className="text-lg font-medium">Powerley</h3>
+                  <span className="block">Web Developer</span>
+                  <time dateTime={new Date(2016, 4, 4).toISOString()}>
+                    May 2016
+                  </time>{" "}
+                  - <time dateTime={data.date}>Present</time>
+                  <ul className="pl-6 list-disc">
+                    <li>First member of the web team</li>
+                    <li>
+                      Created and maintained a suite of white label web
+                      experiences included in our mobile apps
+                    </li>
+                    <li>
+                      Implemented a suite of utility functions used across our
+                      web experiences
+                    </li>
+                    <li>Designed Sketch plugins</li>
+                  </ul>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
@@ -99,4 +108,4 @@ const IndexPage: React.VFC = () => {
 };
 
 export default IndexPage;
-export { meta };
+export { meta, loader, headers };

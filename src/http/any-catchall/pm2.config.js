db039e113f251e7cc7a97730b901fdda0e72831e
Logan McAnsh <logan@mcan.sh>
2021-01-22 18:41:57 -0500

    chore: add arc sandbox to pm2
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- src/http/any-catchall/pm2.config.js
+++ src/http/any-catchall/pm2.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   apps: [
     {
+      name: "Arc",
+      script: "cd ../../../ && arc sandbox",
+      ignore_watch: ["."],
+      env: {
+        NODE_ENV: "development",
+      },
+    },
+    {
       name: "Remix",
       script: "remix run",
       ignore_watch: ["."],
